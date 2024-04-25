a8572ed51e121f1d0082d8533d5190200510d740
TiagoEstre <145720543+TiagoEstre@users.noreply.github.com>
2024-04-25 20:57:38 +0100

    Add project files.

---
+++ Projeto.TS/views/Interface.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Projeto.TS
+{
+    partial class Interface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interface));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.global = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBoxChat = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBoxMesagem = new System.Windows.Forms.TextBox();
+            this.pictureBoxSendz = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.global)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChat)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSendz)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(22)))), ((int)(((byte)(32)))));
+            this.flowLayoutPanel1.Controls.Add(this.global);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(75, 775);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // global
+            // 
+            this.global.BackColor = System.Drawing.Color.Transparent;
+            this.global.Image = ((System.Drawing.Image)(resources.GetObject("global.Image")));
+            this.global.Location = new System.Drawing.Point(16, 15);
+            this.global.Margin = new System.Windows.Forms.Padding(15);
+            this.global.Name = "global";
+            this.global.Size = new System.Drawing.Size(44, 44);
+            this.global.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.global.TabIndex = 1;
+            this.global.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(22)))), ((int)(((byte)(32)))));
+            this.panel2.Controls.Add(this.pictureBoxChat);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(992, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(267, 775);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBoxChat
+            // 
+            this.pictureBoxChat.BackColor = System.Drawing.Color.White;
+            this.pictureBoxChat.Location = new System.Drawing.Point(37, 15);
+            this.pictureBoxChat.Name = "pictureBoxChat";
+            this.pictureBoxChat.Size = new System.Drawing.Size(199, 192);
+            this.pictureBoxChat.TabIndex = 9;
+            this.pictureBoxChat.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(75, 39);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(327, 736);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBoxMesagem);
+            this.panel4.Controls.Add(this.pictureBoxSendz);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(402, 721);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(590, 54);
+            this.panel4.TabIndex = 5;
+            // 
+            // textBoxMesagem
+            // 
+            this.textBoxMesagem.Location = new System.Drawing.Point(10, 15);
+            this.textBoxMesagem.Multiline = true;
+            this.textBoxMesagem.Name = "textBoxMesagem";
+            this.textBoxMesagem.Size = new System.Drawing.Size(526, 24);
+            this.textBoxMesagem.TabIndex = 8;
+            this.textBoxMesagem.Text = "ESCREVER A MENSAGENS";
+            // 
+            // pictureBoxSendz
+            // 
+            this.pictureBoxSendz.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.pictureBoxSendz.Location = new System.Drawing.Point(545, 15);
+            this.pictureBoxSendz.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBoxSendz.Name = "pictureBoxSendz";
+            this.pictureBoxSendz.Size = new System.Drawing.Size(42, 26);
+            this.pictureBoxSendz.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSendz.TabIndex = 8;
+            this.pictureBoxSendz.TabStop = false;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.ForeColor = System.Drawing.Color.White;
+            this.labelName.Location = new System.Drawing.Point(383, 9);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(204, 20);
+            this.labelName.TabIndex = 10;
+            this.labelName.Text = "NAME CLIENT / SERVER";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(25)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.labelName);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(75, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(917, 39);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(335, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(47)))));
+            this.ClientSize = new System.Drawing.Size(1259, 775);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Interface";
+            this.Text = "Interface";
+            this.Load += new System.EventHandler(this.Interface_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.global)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChat)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSendz)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox global;
+        private System.Windows.Forms.TextBox textBoxMesagem;
+        private System.Windows.Forms.PictureBox pictureBoxSendz;
+        private System.Windows.Forms.PictureBox pictureBoxChat;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
