337491b28bc7b9cf8b1a4b2f7b94bd7d115c4639
TiagoEstre <145720543+TiagoEstre@users.noreply.github.com>
2024-05-03 19:24:56 +0100

    Add files via upload

---
+++ Projecto_TS/views/HomePage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projecto_TS.views
+{
+    public partial class HomePage : Form
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxDefinition_Click(object sender, EventArgs e)
+        {
+            Definition definition = new Definition();
+
+            definition.ShowDialog();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string mesage = textBoxMesagem.Text;
+
+            listBoxMessager.Text = mesage;
+        }
+    }
+}
