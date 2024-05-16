c0279525a8e782565c44cf14b60d3a2f8bff71bc
Ivan Santos <146388571+ivanfsantos@users.noreply.github.com>
2024-05-16 19:22:28 +0100

    form das definições com buttons e uma imagem

--- Projecto_TS/views/Definition.Designer.cs
+++ Projecto_TS/views/Definition.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Projecto_TS.views
 {
-    partial class Definition
+    partial class buttonVoltar
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(buttonVoltar));
+            this.buttonTemas = new System.Windows.Forms.Button();
+            this.buttonNotificacoes = new System.Windows.Forms.Button();
+            this.buttonContas = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Definition
+            // buttonTemas
+            // 
+            this.buttonTemas.Location = new System.Drawing.Point(48, 174);
+            this.buttonTemas.Name = "buttonTemas";
+            this.buttonTemas.Size = new System.Drawing.Size(105, 45);
+            this.buttonTemas.TabIndex = 0;
+            this.buttonTemas.Text = "Temas";
+            this.buttonTemas.UseVisualStyleBackColor = true;
+            // 
+            // buttonNotificacoes
+            // 
+            this.buttonNotificacoes.Location = new System.Drawing.Point(48, 239);
+            this.buttonNotificacoes.Name = "buttonNotificacoes";
+            this.buttonNotificacoes.Size = new System.Drawing.Size(105, 41);
+            this.buttonNotificacoes.TabIndex = 1;
+            this.buttonNotificacoes.Text = "Notificações";
+            this.buttonNotificacoes.UseVisualStyleBackColor = true;
+            // 
+            // buttonContas
+            // 
+            this.buttonContas.Location = new System.Drawing.Point(48, 306);
+            this.buttonContas.Name = "buttonContas";
+            this.buttonContas.Size = new System.Drawing.Size(105, 38);
+            this.buttonContas.TabIndex = 2;
+            this.buttonContas.Text = "Contas";
+            this.buttonContas.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(48, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 42);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Voltar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(503, 315);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(219, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(515, 343);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // buttonVoltar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Definition";
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonContas);
+            this.Controls.Add(this.buttonNotificacoes);
+            this.Controls.Add(this.buttonTemas);
+            this.Name = "buttonVoltar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Definition";
+            this.Text = "Definitions";
+            this.Load += new System.EventHandler(this.Definition_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonTemas;
+        private System.Windows.Forms.Button buttonNotificacoes;
+        private System.Windows.Forms.Button buttonContas;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
